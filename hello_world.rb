4cef629d05be6ec7bc2394b430beca90ff3d4efe
MitchGianoni <mitchgianoni@yahoo.com>
2018-05-24 20:50:32 -0400

    first commit - solved

---
+++ hello_world.rb
@@ -0,0 +1,14 @@
+class HelloWorld 
+	def self.hello(name_to_use=nil)
+		if name_to_use
+			"Hello, #{name_to_use}!"
+		else
+			"Hello, World!"
+		end
+	end
+end
+
+puts "Please enter your name so that we can greet you more personally:"
+name = gets.chomp
+
+puts HelloWorld.hello(name)
