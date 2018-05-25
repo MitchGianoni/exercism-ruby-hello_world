dfb640e2f3dbe036073a59f0dc572d605d6824b8
MitchGianoni <mitchgianoni@yahoo.com>
2018-05-24 21:05:31 -0400

    converted indentation from tab to two spaces

--- hello_world.rb
+++ hello_world.rb
@@ -1,11 +1,11 @@
-class HelloWorld 
-	def self.hello(name_to_use=nil)
-		if name_to_use
-			"Hello, #{name_to_use}!"
-		else
-			"Hello, World!"
-		end
-	end
+class HelloWorld
+  def self.hello(name_to_use=nil)
+    if name_to_use
+      "Hello, #{name_to_use}!"
+    else
+      "Hello, World!"
+    end
+  end
 end
 
 puts "Please enter your name so that we can greet you more personally:"
